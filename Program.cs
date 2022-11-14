199d8743d1258c77a91ed13eec519ed64cf4ea4d
mafik_tolik <mafik_tolik@mail.ru>
2022-11-14 15:43:12 +0300

    Created the FillArray and PrintArray methods

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+TaskSolution();
+
+void TaskSolution()
+{
+    Console.WriteLine("Введите размер массива:");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    string[] text = new string[size];
+
+    FillArray(text);
+    PrintArray(text);
+
+    void FillArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Введите размер массива:");
+    }
+
+    void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(string.Join(", ", array));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
