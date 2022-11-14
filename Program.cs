734f16164ac4cada66adc2514f890dec5270c554
mafik_tolik <mafik_tolik@mail.ru>
2022-11-14 17:06:41 +0300

    Added a method for filling in the final array

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿Console.Clear();
-
-TaskSolution();
+﻿TaskSolution();
 
 void TaskSolution()
 {
@@ -9,31 +7,49 @@ void TaskSolution()
 
     string[] text = new string[size1];
 
-    FillArray(text);
+    FillArray1(text);
+    Console.WriteLine($"Вывод заданного массива: [{string.Join(", ", text)}]");
 
-    void FillArray(string[] array)
+    int limitation = 4;
+
+    int size2 = Count(text);
+    string[] threeSymbols = new string[size2];
+
+    FillArray2(text, threeSymbols);
+    Console.WriteLine($"Вывод итогового массива: [{string.Join(", ", threeSymbols)}]");
+
+
+
+    void FillArray1(string[] array)
     {
         for (int i = 0; i < array.Length; i++)
         {
+            Console.WriteLine($"Введите значение {i + 1}-го элемента массива:");
             array[i] = Console.ReadLine();
             if (array[i] == null) array[i] = String.Empty;
         }
     }
 
-    Console.WriteLine($"Вывод первоначального массива: [{string.Join(", ", text)}]");
-
     int Count(string[] array)
     {
         int count = 0;
         for (int i = 0; i < array.Length; i++)
-            if (array[i].Length < 4) count++;
+            if (array[i].Length < limitation) count++;
         return count;
     }
 
-    int size2 = Count(text);
-    string[] threeSymbols = new string[size2];
-
-    Console.WriteLine("Размер второго массива: " + size2);
+    void FillArray2(string[] array1, string[] array2)
+    {
+        int j = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i].Length < limitation)
+            {
+                array2[j] = array1[i];
+                j++;
+            }
+        }
+    }
 }
 
 
