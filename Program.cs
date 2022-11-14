57ee1dc1a6f46eb25f96959d400a64a797cb30c8
mafik_tolik <mafik_tolik@mail.ru>
2022-11-14 16:27:38 +0300

    Added a method to find the size of the second array

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ TaskSolution();
 void TaskSolution()
 {
     Console.WriteLine("Введите размер массива:");
-    int size = Convert.ToInt32(Console.ReadLine());
+    int size1 = Convert.ToInt32(Console.ReadLine());
 
-    string[] text = new string[size];
+    string[] text = new string[size1];
 
     FillArray(text);
 
@@ -16,11 +16,24 @@ void TaskSolution()
         for (int i = 0; i < array.Length; i++)
         {
             array[i] = Console.ReadLine();
+            if (array[i] == null) array[i] = String.Empty;
         }
     }
 
-    Console.WriteLine($"Вывод массива: [{string.Join(", ", text)}]");
+    Console.WriteLine($"Вывод первоначального массива: [{string.Join(", ", text)}]");
 
+    int Count(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            if (array[i].Length < 4) count++;
+        return count;
+    }
+
+    int size2 = Count(text);
+    string[] threeSymbols = new string[size2];
+
+    Console.WriteLine("Размер второго массива: " + size2);
 }
 
 
